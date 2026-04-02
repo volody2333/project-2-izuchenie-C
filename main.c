889d738e2babe3e5501d019f0b77ce69834b278f
volody2333 <volody2333@mail.ru>
2026-04-02 21:44:58 +1000

    Понял как работает операции присваиваня и инициализация переменных.

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// релю: ноепюжхх опхябюхбюмхъ
+
+int main(void)
+{
+	char ch;
+	ch = 'd';
+	printf("ch = %c, code = %d\n", ch, ch);
+	
+	double d1, d2, d3, d4;
+	float var_f;
+	var_f = 10.0f;
+	d1 = 10.0f;
+	d2 = -7.;
+	d3 = 1e2;
+	d4 = 5e-3;
+	
+	int size_float = sizeof(float);
+	
+	return 0;
+}
+// char хяонкэгсеряъ рнкэйн я яхлбнкюлх
+// 5e-3 щрн 5*10 б лхмся рперэеи яреоемх = 0,005
